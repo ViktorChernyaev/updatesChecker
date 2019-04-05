bc9f7928139c17e549fcece391b94b8eaed41d99
ViktorChernyaev <bagunok18@gmail.com>
2019-04-05 21:03:33 +0300

    Add logging

--- app.js
+++ app.js
@@ -4,11 +4,13 @@ const TelegramBot = require("node-telegram-bot-api");
 const axios = require("axios");
 
 const { TELEGRAM_BOT_TOKEN } = process.env;
+  console.log(TELEGRAM_BOT_TOKEN);
 
 const bot = new TelegramBot(TELEGRAM_BOT_TOKEN, { polling: true });
 
 bot.on(/\/echo (.+)/, (msg, match) => {
   const chatId = msg.chat.id;
   const resp = match[1];
+  console.log(322)
   bot.sendMessage(chatId, resp);
 })
