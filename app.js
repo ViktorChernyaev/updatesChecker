244aa753cdea905e789e80697d87511761fb915d
ViktorChernyaev <bagunok18@gmail.com>
2019-04-05 21:46:26 +0300

    Add scheduler

--- app.js
+++ app.js
@@ -1,24 +1,20 @@
 const TelegramBot = require("node-telegram-bot-api");
 const axios = require("axios");
+const cron = require("node-cron");
 
-const { TELEGRAM_BOT_TOKEN, TELEGRAM_GROUP_ID, GITHUB_REPO_NAME } = process.env;
+const { TELEGRAM_BOT_TOKEN, TELEGRAM_GROUP_ID, GITHUB_REPO_NAME, CRON_SCHEDULE } = process.env;
 
 const bot = new TelegramBot(TELEGRAM_BOT_TOKEN, { polling: true });
 
 function sendCountToGroup(count = 1) {
-  return bot.sendMessage(TELEGRAM_GROUP_ID, count);
+  return bot.sendMessage(TELEGRAM_GROUP_ID, `996icu now have ${count} stars`);
 }
 
-//1001126293430 target froup
-
-const headers = {
-  "Accept": "application/vnd.github.v3+json"
-};
-
+const headers = { "Accept": "application/vnd.github.v3+json" };
 function getStarsCount() {
   return axios.get(`https://api.github.com/repos/${GITHUB_REPO_NAME}`, { headers })
     .then(({ data }) => data.stargazers_count)
     .catch(e => console.log(e));
 }
 
-getStarsCount().then(sendCountToGroup);
+cron.schedule(CRON_SCHEDULE, () => getStarsCount().then(sendCountToGroup));
