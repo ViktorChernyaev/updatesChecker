e37820b56be11b2ed6c6b91799d3583ca846e028
ViktorChernyaev <bagunok18@gmail.com>
2019-04-05 20:55:13 +0300

    Configure dotenv

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-require("dotenv").config();
+require("dotenv").config({ silent: process.env.NODE_ENV === "production" });
 
 const TelegramBot = require("node-telegram-bot-api");
 const axios = require("axios");
