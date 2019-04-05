aacf8758f241c661d96c4f05603315c1f7ffd18d
ViktorChernyaev <bagunok18@gmail.com>
2019-04-05 21:41:35 +0300

    Add request

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
-// require("dotenv").config({ silent: process.env.NODE_ENV === "production" });
-
 const TelegramBot = require("node-telegram-bot-api");
 const axios = require("axios");
 
-const { TELEGRAM_BOT_TOKEN } = process.env;
-  console.log(TELEGRAM_BOT_TOKEN);
+const { TELEGRAM_BOT_TOKEN, TELEGRAM_GROUP_ID, GITHUB_REPO_NAME } = process.env;
 
 const bot = new TelegramBot(TELEGRAM_BOT_TOKEN, { polling: true });
 
-bot.sendMessage(131075441, "check");
+function sendCountToGroup(count = 1) {
+  return bot.sendMessage(TELEGRAM_GROUP_ID, count);
+}
+
+//1001126293430 target froup
+
+const headers = {
+  "Accept": "application/vnd.github.v3+json"
+};
+
+function getStarsCount() {
+  return axios.get(`https://api.github.com/repos/${GITHUB_REPO_NAME}`, { headers })
+    .then(({ data }) => data.stargazers_count)
+    .catch(e => console.log(e));
+}
+
+getStarsCount().then(sendCountToGroup);
